6afb7a5eb7950ecca4e8f92bc609d26fe9ddd227
rouaa2003 <q1u2e3e4n5j6o7@gmail.com>
2025-01-29 10:33:07 +0300

    firstJstest

---
+++ main.js
@@ -0,0 +1,8 @@
+
+// document.querySelector("h1").style.color='brown'
+
+// window.alert("100*7") 
+document.write("<h1>hello</h1>")
+document.write("test test")
+console.log(58*6)
+// document.createElement("<h3>hhi<h3>")
